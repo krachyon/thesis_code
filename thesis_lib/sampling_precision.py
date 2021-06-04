039a5e048086cd3b9d7a65d9a53b3685668b465d
Sebastian <sebastian.messlinger@posteo.de>
2021-06-04 14:58:22 +0200

    switch to dictionary return

--- thesis_lib/sampling_precision.py
+++ thesis_lib/sampling_precision.py
@@ -23,25 +23,56 @@ from astropy.modeling.functional_models import Gaussian2D, AiryDisk2D, Moffat2D,
 import numpy as np
 import numbers
 from astropy.table import Table
-from typing import Type, Union, List
+from typing import Type, Union, List, Optional
 import matplotlib.pyplot as plt
 from astropy.modeling import fitting
 import multiprocess as mp
 from matplotlib.cm import get_cmap, ScalarMappable
 from matplotlib.colors import Normalize
-from collections import namedtuple
 import os
 from anisocado import AnalyticalScaoPsf
+from itertools import product
 
 
-Result = namedtuple('Result',
-                    ['img', 'n_sources1d',
-                     'tab', 'pixelphase', 'fitshape', 'noise',
-                     'model_oversampling', 'model_degree', 'model_mode', 'model_name',
-                     'fit_accuracy'])
+class Repr:
+    def __repr__(self):
+        items = [item for item in self.__dict__.items() if not item[0].startswith('__')]
+        item_string = ', '.join([f'{item[0]} = {item[1]}' for item in items])
+        return f'{type(self)}({item_string})'
 
 
-def gen_image(model, N1d, size, border, pixelphase, noise=None):
+class Noise(Repr):
+    def __call__(self, data):
+        raise NotImplementedError
+
+
+class GaussNoise(Noise):
+    def __init__(self, center, std):
+        self.center = center
+        self.std = std
+
+    def __call__(self, data):
+        return data + np.random.normal(self.center, self.std, data.shape)
+
+
+class PoissonNoise(Noise):
+    def __init__(self, max_count):
+        self.max_count = max_count
+
+    def __call__(self, data):
+        return np.random.poisson(self.max_count*data)/self.max_count
+
+
+class CombinedNoise(Noise):
+    def __init__(self, center, std, max_count):
+        self.gauss = GaussNoise(center, std)
+        self.poisson = PoissonNoise(max_count)
+
+    def __call__(self, data):
+        return self.gauss(self.poisson(data))
+
+
+def gen_image(model, N1d, size, border, pixelphase, noise: Optional[Noise] = None):
     # TODO could be improved by model.render
     yx_sources = np.mgrid[0+border:size-border:N1d*1j, 0+border:size-border:N1d*1j].transpose((1, 2, 0)).reshape(-1, 2)
     # swap x and y and round to nearest integer
@@ -88,7 +119,6 @@ class DummyAdd2D(Fittable2DModel):
         return np.zeros_like(x)
 
 
-
 def fit_models(input_model: Fittable2DModel,
                pixelphase=0.,
                n_sources1d=4,
@@ -99,7 +129,7 @@ def fit_models(input_model: Fittable2DModel,
                model_oversampling=2,
                model_degree=5,
                model_mode='grid',
-               fit_accuracy=1e-8) -> Result:
+               fit_accuracy=1e-8) -> dict:
 
     # it's pretty hard to get a different random state in each process...
     np.random.seed(int.from_bytes(os.urandom(4), 'little'))
@@ -145,6 +175,10 @@ def fit_models(input_model: Fittable2DModel,
     fitter = fitting.LevMarLSQFitter()
     y, x = np.indices(img.shape)
 
+    # TODO why does this not work?
+    #weights = np.sqrt(img-np.min(img))
+    #weights /= np.sum(weights)/weights.size
+
     res = Table(names='x, y, x_fit, y_fit, flux_fit'.split(', '))
     for xy_position in xy_sources:
         cutout_slices = get_cutout_slices(xy_position, fitshape)
@@ -156,31 +190,26 @@ def fit_models(input_model: Fittable2DModel,
         setattr(fit_model.left, yname, xy_position[1] + np.random.uniform(-0.1, 0.1))
         #getattr(fit_model.left, yname).bounds = (xy_position[1]-1.1, xy_position[1]+1.1)
 
-        fitted = fitter(fit_model, x[cutout_slices], y[cutout_slices], img[cutout_slices], acc=fit_accuracy, maxiter=100_000)
+        fitted = fitter(fit_model, x[cutout_slices], y[cutout_slices], img[cutout_slices],
+                        acc=fit_accuracy, maxiter=100_000)#, weights=weights[cutout_slices])
         res.add_row((xy_position[0],
                      xy_position[1],
                      getattr(fitted.left, xname),
                      getattr(fitted.left, yname),
                      getattr(fitted.left, fluxname)))
-
+    # TODO this nested table stuff is shit
     res['x_dev'] = res['x']-res['x_fit']
     res['y_dev'] = res['y']-res['y_fit']
     res['dev']   = np.sqrt(res['x_dev']**2 + res['y_dev']**2)
-    return Result(img,
-                  n_sources1d,
-                  res,
-                  pixelphase,
-                  fitshape,
-                  noise,
-                  model_oversampling,
-                  model_degree,
-                  model_mode,
-                  type(input_model).__name__,
-                  fit_accuracy)
+    return {'img': img, 'tab': res}
 
 
 def fit_models_dictarg(kwargs_dict):
-    return fit_models(**kwargs_dict)
+    return kwargs_dict | fit_models(**kwargs_dict)
+
+
+def dictoflists_to_listofdicts(dict_of_list):
+    return (dict(zip(dict_of_list.keys(), vals)) for vals in product(*dict_of_list.values()))
 
 
 class AnisocadoModel(FittableImageModel):
@@ -194,59 +223,25 @@ def make_anisocado_model(oversampling=8):
     return AnisocadoModel(img, oversampling=oversampling, degree=3)
 
 
-class Repr:
-    def __repr__(self):
-        items = [item for item in self.__dict__.items() if not item[0].startswith('__')]
-        item_string = ', '.join([f'{item[0]} = {item[1]}' for item in items])
-        return f'{type(self)}({item_string})'
-
-
-class GaussNoise(Repr):
-    def __init__(self, center, std):
-        self.center = center
-        self.std = std
-
-    def __call__(self, data):
-        return data + np.random.normal(self.center, self.std, data.shape)
-
-
-class PoissonNoise(Repr):
-    def __init__(self, max_count):
-        self.max_count = max_count
-
-    def __call__(self, data):
-        return np.random.poisson(self.max_count*data)/self.max_count
-
-
-class CombinedNoise(Repr):
-    def __init__(self, center, std, max_count):
-        self.gauss = GaussNoise(center, std)
-        self.poisson = PoissonNoise(max_count)
-
-    def __call__(self, data):
-        return self.gauss(self.poisson(data))
-
-
-def plot_xy_deviation(results: List[Result], model):
+def plot_xy_deviation(results: List[dict]):
     cmap = get_cmap('turbo')
     plt.figure()
-    plt.title(repr(model))
+    plt.title(repr(results[0]['input_model']))
     for res in results:
-        phase = np.sqrt(res.pixelphase[0]**2+res.pixelphase[1]**2)
-        plt.plot(res.tab['x_dev'], res.tab['y_dev'], 'o', markeredgewidth=0, color=cmap(phase/np.sqrt(2)), alpha=0.5)
+        phase = np.sqrt(res['pixelphase'][0]**2+res['pixelphase'][1]**2)
+        plt.plot(res['tab']['x_dev'], res['tab']['y_dev'], 'o', markeredgewidth=0, color=cmap(phase/np.sqrt(2)), alpha=0.5)
         plt.xlabel('x deviation')
         plt.ylabel('y deviation')
     plt.gca().axis('equal')
     plt.colorbar(ScalarMappable(norm=Normalize(0, np.sqrt(2)), cmap=cmap)).set_label('euclidean pixelphase')
 
 
-def plot_phase_vs_deviation(results: List[Result], model):
+def plot_phase_vs_deviation(results: List[dict]):
     plt.figure()
-    plt.title(repr(model))
+    plt.title(repr(results[0]['input_model']))
 
-
-    phases = [np.sqrt(res.pixelphase[0]**2+res.pixelphase[1]**2) for res in results]
-    sigmas = [np.mean(res.tab['dev']) for res in results]
+    phases = [np.sqrt(res['pixelphase'][0]**2+res['pixelphase'][1]**2) for res in results]
+    sigmas = [np.mean(res['tab']['dev']) for res in results]
     plt.xlabel('euclidean pixelphase')
     plt.ylabel('euclidean xy-deviation')
 
@@ -255,13 +250,13 @@ def plot_phase_vs_deviation(results: List[Result], model):
     plt.plot(phases[order], sigmas[order])
 
 
-def plot_phase_vs_deviation3d(results: List[Result], model):
+def plot_phase_vs_deviation3d(results: List[dict]):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-    ax.set_title(repr(model))
+    ax.set_title(repr(results[0]['input_model']))
 
-    phases_3d = np.array([res.pixelphase for res in results])
-    sigmas = [np.mean(res.tab['dev']) for res in results]
+    phases_3d = np.array([res['pixelphase'] for res in results])
+    sigmas = [np.mean(res['tab']['dev']) for res in results]
 
     fig.tight_layout()
     ax.plot_trisurf(phases_3d[:, 0], phases_3d[:, 1], sigmas, cmap='gist_earth')
@@ -270,44 +265,75 @@ def plot_phase_vs_deviation3d(results: List[Result], model):
     ax.set_zlabel('euclidean  deviation')
 
 
-def plot_fitshape(results: List[Result]):
-    dat = Table(rows=[(np.mean(res.tab['dev']), res.fitshape[0], res.model_name) for res in results],
-              names=['dev','fitshape','model_name']).group_by(['fitshape','model_name'])
+def plot_fitshape(results: List[dict]):
+    dat = Table(rows=[(np.mean(res['tab']['dev']), res['fitshape'][0], res['model_name']) for res in results],
+              names=['dev', 'fitshape', 'model_name']).group_by(['fitshape', 'model_name'])
 
     mean = dat.groups.aggregate(np.mean).group_by('model_name').groups
     std = dat.groups.aggregate(np.std).group_by('model_name').groups
 
 
-    fig, axes = plt.subplots(len(mean), sharex=True)
+    fig, axes = plt.subplots(len(mean), sharex='True')
     for i in range(len(mean)):
         axes[i].errorbar(mean[i]['fitshape'], mean[i]['dev'], std[i]['dev'], fmt='o', label=mean.keys[i]['model_name'])
         axes[i].legend()
 
 
-if __name__ == '__main__':
+def plot_deviation_vs_noise(results: List[dict], noise_type=GaussNoise):
+    if noise_type == GaussNoise:
+        rows = [(np.abs(res['tab']['dev']), res['noise'].std, repr(res['input_model']))
+                for res in results if isinstance(res['noise'], noise_type)]
+    elif noise_type == PoissonNoise:
+        rows = [(np.abs(res['tab']['dev']), res['noise'].max_count, repr(res['input_model']))
+                for res in results if isinstance(res['noise'], noise_type)]
 
-    model = Gaussian2D(x_stddev=2., y_stddev=2.)  # ,theta=2*np.pi/8)
-    # model = AiryDisk2D(radius=4.5)
+    dat = Table(rows=rows, names=['dev', 'noise', 'input_model']).group_by(['input_model', 'noise'])
+
+    mean = dat.groups.aggregate(lambda x: np.mean(x)).group_by('input_model').groups
+    std  = dat.groups.aggregate(np.std).group_by('input_model').groups
+
+    plt.figure()
+    for i in range(len(mean)):
+        plt.errorbar(mean[i]['noise'], mean[i]['dev'], std[i]['dev'], fmt='o', label=mean.keys[i]['input_model'])
+    plt.legend()
+    plt.xscale('log')
+    plt.yscale('log')
+    if noise_type == GaussNoise:
+        plt.xlabel('Gaussian Noise σ')
+    elif noise_type == PoissonNoise:
+        plt.xlabel('Poisson λ')
+    plt.ylabel('Deviation in measured Position')
+    plt.title('everythings a line on a loglog-plot lol')
+
+
+if __name__ == '__main__':
+    from tqdm.cli import tqdm
+    #model = Gaussian2D(x_stddev=4.7, y_stddev=4.7)  # ,theta=2*np.pi/8)
+    model = AiryDisk2D(radius=4.5)
     # model = Moffat2D(gamma=2.5, alpha=2.5)
     # model = make_anisocado_model()
 
-    size_1D = 5j
+    size_1D = 70j
     xy_pairs = np.mgrid[0:1:size_1D, 0:1:size_1D].transpose((1, 2, 0)).reshape(-1, 2)
+    dl = {'input_model': [model],
+          'n_sources1d': [1],
+          'img_border': [32],
+          'img_size': [64],
+          'pixelphase': xy_pairs,
+          'fitshape': [(21, 21)],
+          'noise': [None],
+          'model_oversampling': [2],
+          'model_degree': [3],
+          'model_mode': ['grid'],
+          'fit_accuracy': [1.49012e-08]
+          }
 
-    from thesis_lib.util import DebugPool
-    with DebugPool() as p:
-    #with mp.Pool(10) as p:
-        results = p.map(lambda phase: fit_models(input_model=model,
-                                                 pixelphase=phase,
-                                                 fitshape=(31, 31),
-                                                 model_oversampling=2,
-                                                 model_degree=5,
-                                                 model_mode='same',
-                                                 fit_accuracy=1e-10,
-                                                 noise=lambda data: CombinedNoise(0, 1e-10, 20)(data)),
-                        xy_pairs)
+    #from thesis_lib.util import DebugPool
+    #with DebugPool() as p:
+    with mp.Pool(10) as p:
+        results = list(p.imap_unordered(fit_models_dictarg, tqdm(list(dictoflists_to_listofdicts(dl)))))
 
-    plot_xy_deviation(results, model)
-    plot_phase_vs_deviation(results, model)
-    plot_phase_vs_deviation3d(results, model)
+    plot_xy_deviation(results)
+    plot_phase_vs_deviation(results)
+    plot_phase_vs_deviation3d(results)
     plt.show()
